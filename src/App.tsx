6cab38778f19470b4b98987ed784a61c582b8ec2
anthony62490 <anthonyjmagno@gmail.com>
2018-11-26 23:30:47 -0600

    fixed wrong name

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">PicSort v2</h1>
+          <h1 className="App-title">PicTagger v2</h1>
         </header>
       </div>
     );
