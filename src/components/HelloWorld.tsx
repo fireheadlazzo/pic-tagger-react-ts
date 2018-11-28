dc265b6f1f059ea009417fb66dedd4b62a57db26
anthony62490 <anthonyjmagno@gmail.com>
2018-11-27 19:57:44 -0600

    changed rules to allow arrow functions

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as React from 'react';
 
 class HelloWorld extends React.Component<any, any> {
@@ -6,13 +7,20 @@ class HelloWorld extends React.Component<any, any> {
     super(props);
     this.state = 
     {
-      print: this.props.phrase
+      image: "",
+      print: this.props.phrase,
+      target: null
     };
   }
   public render() {
     return (
       <div>
+        <input 
+          onChange={(e) => this.setState({target: e.target.value})}
+          onSubmit={() => this.setState({image: axios.get(`/api/photos/${this.state.target}`)})}
+        />
         {this.state.print}
+        <img src={this.state.image}/>
       </div>
     );
   }
