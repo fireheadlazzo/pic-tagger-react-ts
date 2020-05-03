42118f9cf792fc45b69a71f759de6229aeee5553
anthony62490 <anthonyjmagno@gmail.com>
2020-05-02 20:10:43 -0500

    importing new db scripts

---
+++ db/200427-create-photo-table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS public.photo
+(
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    url VARCHAR(128) NOT NULL,
+    title VARCHAR(128) DEFAULT '',
+    uid BIGINT NOT NULL,
+    tags JSONB DEFAULT '[]' NOT NULL,
+    details JSONB DEFAULT '{}' NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    created_by VARCHAR(128) NOT NULL,
+    updated_by VARCHAR(128) NOT NULL
+);
+
+CREATE INDEX photo_tags_gin_idx
+    ON public.photo
+        USING GIN (tags);
