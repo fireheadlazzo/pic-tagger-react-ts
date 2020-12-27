e9827f9d330e2bde97d002fd2f81400861da59b6
anthony62490 <anthonyjmagno@gmail.com>
2020-12-26 20:26:43 -0600

    integrating google storage

--- server/src/config.ts
+++ server/src/config.ts
@@ -8,12 +8,13 @@ nconf.env().argv(); // TODO: What does this do?
 nconf.file({file: path.join(__dirname, "config.json")});
 
 nconf.defaults({
+    IMAGE_BUCKET: "",
     PORT: 8000,
     SQL_USER: "postgres",
     SQL_DATABASE: "postgres",
     SQL_HOST: "",
     SQL_PASSWORD: "",
-    SQL_PORT: "5432",
+    SQL_PORT: "1234",
 });
 
 export default nconf;
