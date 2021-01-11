6b1d04a05f651160157c4e5d5daca95d3a4a860d
fireheadlazzo <anthonyjmagno@gmail.com>
2021-01-11 02:02:43 -0600

    router and error cleanup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,19 @@
 import "module-alias/register";
 import express from "express";
+// import { Request, Response, NextFunction } from "express";
 import config from "./config";
 import router from "./router/router";
+import morgan from "morgan";
+// import * as constants from "./models/constants";
+// import { StatusError } from "./models/status-error";
 
 const app = express();
-app.use(router.app);
-const PORT = config.get("PORT");
+app.use(morgan("dev"));
+app.use(express.json());
 
+app.use(router);
+
+const PORT = config.get("PORT");
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
   console.log(`SQL_PORT: ${config.get("SQL_PORT")}`);
--- server/src/router/router.ts
+++ server/src/router/router.ts
@@ -1,50 +1,39 @@
 import express from "express";
 import * as storage from "../services/cloudstorage";
-import morgan from "morgan";
 import { createImage, getImage } from "../controllers/images";
 import { createTag } from "../controllers/tags";
 import * as middleware from "../middlewares";
 import * as constants from "../models/constants";
 
-class Router {
-  constructor() {
-    this.app = express.Router();
-    this.app.use(express.json());
+const app = express.Router();
 
-    this.app.use(morgan("dev"));
+console.log("Defining routes");
+/**
+ * Images
+ */
+app.get(`/${constants.imagesRoute}/:id`, getImage);
 
-    // Assign the following routes
-    console.log("Defining routes");
-    /**
-     * Images
-     */
-    this.app.get(`/${constants.imagesRoute}/:id`, getImage);
+app.post(`/${constants.imagesRoute}/?`,
+  storage.multer.single("file"),
+  middleware.checkKeys.images.POST,
+  storage.sendImageToGCS,
+  createImage
+);
+/**
+ * Tags
+ */
+app.get(`/${constants.tagsRoute}/?`,
+  middleware.checkKeys.tags.GET
+);
+app.post(`/${constants.tagsRoute}/?`,
+  middleware.checkKeys.tags.POST,
+  createTag
+);
 
-    this.app.post(`/${constants.imagesRoute}/?`,
-      storage.multer.single("file"),
-      middleware.checkKeys.images.POST,
-      storage.sendImageToGCS,
-      createImage
-    );
-    /**
-     * Tags
-     */
-    this.app.post(`/${constants.tagsRoute}/?`,
-      middleware.checkKeys.tags.POST,
-      createTag
-    );
-    this.app.get(`/${constants.tagsRoute}/?`,
-      middleware.checkKeys.tags.GET
-    );
+app.use(middleware.jsonErrorHandler);
+app.use((req, res, next) => {
+  res.set("Content-Type", "application/json");
+  next();
+});
 
-    this.app.use(middleware.jsonErrorHandler);
-    this.app.use((req, res, next) => {
-      res.set("Content-Type", "application/json");
-      next();
-    });
-  }
-
-  public app: express.Router;
-}
-
-export default new Router;
+export default app;
