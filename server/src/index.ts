c9fe65f2cf225efc59d4d79aa289a24cc3789134
fireheadlazzo <anthonyjmagno@gmail.com>
2021-01-11 02:41:55 -0600

    handling request body

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
 import "module-alias/register";
 import express from "express";
-// import { Request, Response, NextFunction } from "express";
 import config from "./config";
 import router from "./router/router";
 import morgan from "morgan";
-// import * as constants from "./models/constants";
-// import { StatusError } from "./models/status-error";
+import bp from "body-parser";
 
 const app = express();
 app.use(morgan("dev"));
+app.use(bp.json());
+app.use(bp.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(router);
