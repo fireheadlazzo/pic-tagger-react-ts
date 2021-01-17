ffa1da300641323e6e6d208c6c4f4cd1d7913968
fireheadlazzo <anthonyjmagno@gmail.com>
2021-01-17 11:47:43 -0600

    list tags endpoint

---
+++ server/src/controllers/tags/list.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import * as sql from "services/sql";
+import { Tag } from "models/objs/tag";
+import { StatusError } from "models/status-error";
+import { QueryOptions } from "models/objs/query-options";
+
+export function listTags(req: Request, res: Response, next: NextFunction) {
+  const options = new QueryOptions(req.query);
+  console.log(`Listing tags with options [${options.toString()}]`);
+  // TODO: add options for filtering by tag type?
+
+  return sql.listTags(options)
+  .then((values: Tag[]) => {
+    console.log(`Got [${values.length}] tags`);
+    return res.send(values);
+  })
+  .catch((err: StatusError) => {
+    if (err.status) {
+      return res.status(err.status).send(err.message);
+    }
+    return next(err);
+  });
+}
