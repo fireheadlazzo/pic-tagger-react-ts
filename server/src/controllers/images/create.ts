8e04e81f79fe8619dea5a6a5e9bb01069f1a8f3f
fireheadlazzo <anthonyjmagno@gmail.com>
2022-09-07 01:03:15 -0500

    refactoring Image object creation

--- server/src/controllers/images/create.ts
+++ server/src/controllers/images/create.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from "express";
 import { UploadRequest } from "models/interfaces/upload-request";
 import { Image } from "models/objs/image";
 import * as sql from "services/sql";
+import config from "config";
 
 export function createImage(
   req: Request & UploadRequest,
@@ -10,26 +11,27 @@ export function createImage(
   next: NextFunction
 ) {
   console.log(
-    `Creating new image for file [${req.file?.bucket}/${req.file?.path}]`
+    `Creating new image for file [${config.get("IMAGE_BUCKET")}/${req.file?.filename}]`
   );
   const { file } = req;
-  if (!file || !file.originalname || !file.bucket || !file.path) {
+  if (!file) {
     const error = new Error("Cloud Storage keys not found on file");
     return next(error);
   }
 
-  const item = new Image(file);
-  item.tags = [];
-  item.filename = file.path;
-  item.details = {
-    metadata: {
-      height: 0,
-      width: 0,
-    },
-    uploadInfo: {
-      originalName: file.originalname,
-    },
+  const imageData = {
+    details: {
+      metadata: {
+        height: 0,
+        width: 0,
+        originalName: file.originalname,
+        encoding: file.encoding,
+        mimetype: file.mimetype,
+        size: file.size,
+      },
+    }
   };
+  const item = new Image(imageData, file);
 
   return sql
     .saveImage(item)
